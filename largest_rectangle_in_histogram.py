32c1a0f6510bcf9f1f08a940cae5d6bbbdb889c0
KingTugce <kingstugce@gmail.com>
2023-03-29 01:43:37 -0700

    update

--- largest_rectangle_in_histogram.py
+++ largest_rectangle_in_histogram.py
@@ -23,3 +23,27 @@ def rec(heights, low, high):
         from_left = rec(heights, low, pos_min-1)
         from_right = rec(heights, pos_min+1, high)
         return max(from_left, from_right, minh*(high-low+1))
+def largest_rectangle(heights):
+    return rec(heights, 0, len(heights)-1)
+
+
+def largest_rectangle(heights):
+    heights = [-1]+heights+[-1]
+    from_left = [0]*len(heights)
+    stack = [0]
+    for i in range(1, len(heights)-1):
+        while heights[stack[-1]] >= heights[i]:
+            stack.pop()
+        from_left[i] = stack[-1]
+        stack.append(i)
+    from_right = [0]*len(heights)
+    stack = [len(heights)-1]
+    for i in range(1, len(heights)-1)[::-1]:
+        while heights[stack[-1]] >= heights[i]:
+            stack.pop()
+        from_right[i] = stack[-1]
+        stack.append(i)
+    max_area = 0
+    for i in range(1, len(heights)-1):
+        max_area = max(max_area, heights[i]*(from_right[i]-from_left[i]-1))
+    return max_area
